f25f6e4a7b6206b46cb35a3fbd5d2e38afa2ea5d
Tochukwu <tochukwuowunwanne@gmail.com>
2023-10-14 01:38:36 +0100

    first commit

---
+++ pages/product/[slug].js
@@ -0,0 +1,138 @@
+import React, { useState } from "react";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiFillStar,
+  AiOutlineStar,
+} from "react-icons/ai";
+
+import { client, urlFor } from "../../lib/clients";
+import { Product } from "../../components";
+import { useStateContext } from "../../context/StateContext";
+
+const ProductDetails = ({ product, products }) => {
+  const { image, name, details, price } = product;
+  const [index, setIndex] = useState(0);
+
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+
+    setShowCart(true);
+  }
+  
+  return (
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img
+              src={urlFor(image && image[index])}
+              className="product-detail-image"
+            />
+          </div>
+          <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img
+                key={i}
+                src={urlFor(item)}
+                className={
+                  i === index ? "small-image selected-image" : "small-image"
+                }
+                onMouseEnter={() => setIndex(i)}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="product-detail-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>(20)</p>
+          </div>
+          <h4>Details:</h4>
+          <p>{details}</p>
+          <p className="price">${price}</p>
+          <div className="quantity">
+            <h3>Quantity:</h3>
+            <p className="quantity-desc">
+              <span onClick={decQty} className="minus">
+                <AiOutlineMinus />
+              </span>
+              <span className="num">
+                {qty}
+              </span>
+              <span onClick={incQty} className="plus">
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+
+          <div className="buttons">
+            <button onClick={() => onAdd(product, qty)} type="button" className="add-to-cart">
+              Add to Cart
+            </button>
+            <button onClick={handleBuyNow} type="button" className="buy-now">
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// function to fetch data from sanity
+export const getStaticProps = async ({ params: { slug } }) => {
+  // fetch product
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`; //get the individuala product
+  const productsQuery = '*[_type == "product"]';
+
+  const product = await client.fetch(query); //to fecth individual product
+  const products = await client.fetch(productsQuery);
+
+  return {
+    props: { products, product },
+  };
+};
+
+//When use getStaticProps, you must also use getStaticPaths
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product"] {
+        slug {
+            current
+        }
+    }`;
+
+  const products = await client.fetch(query);
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export default ProductDetails;
